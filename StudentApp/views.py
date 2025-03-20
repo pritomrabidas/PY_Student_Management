77964d7edcddfd12926972b1052105b20e8ed70f
pritom rabidas <pritomrabidas102@gmail.com>
2025-03-20 20:01:56 +0600

    changes

--- StudentApp/views.py
+++ StudentApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Student
+from django.db.models import Q
 def firstFun(request):
     Students = Student.objects.all()
+    if request.method == 'GET':
+        data = request.GET.get('src')
+        if data:
+            Students = Student.objects.filter(Q(name__icontains=data) | Q(email__icontains=data))
     return render(request,'Home/Home.html',{'stu':Students})
